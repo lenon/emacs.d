aa547a771c933a03c33f2e01dfb7cc31d00acc85
Lenon Marcel <lenon.marcel@gmail.com>
2017-03-16 13:49:57 -0300

    Move startup setting to GUI block

--- init.el
+++ init.el
@@ -29,9 +29,9 @@
 ;; GUI settings
 (scroll-bar-mode -1) ; disable scroll bars
 (tool-bar-mode -1) ; disable tool-bar on GUI
+(setq inhibit-startup-screen 1) ; inhibits the startup screen
 
 ;; Editor settings
-(setq inhibit-startup-screen 1) ; inhibits the startup screen
 (setq show-paren-delay 0) ; show matching pairs of perentheses with 0 delay
 (show-paren-mode 1) ; show parentheses
 (setq-default truncate-lines 1) ; truncate lines (no-wrap)
