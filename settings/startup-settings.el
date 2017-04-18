d4d15835261881339fa075d0d6afe1bf019b50a3
Lenon Marcel <lenon.marcel@gmail.com>
2017-04-17 23:17:19 -0300

    Trying to fix a issue with customs

--- settings/startup-settings.el
+++ settings/startup-settings.el
@@ -1,7 +1,7 @@
 (setq inhibit-startup-screen t) ; hide the welcome screen
 
 ;; Disable the startup message in the echo area
-(put 'inhibit-startup-echo-area-message 'saved-value
-     (setq inhibit-startup-echo-area-message (user-login-name)))
+(put 'inhibit-startup-echo-area-message 'saved-value t)
+(setq inhibit-startup-echo-area-message (user-login-name))
 
 (provide 'startup-settings)
