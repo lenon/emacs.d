a298ef623051c71eaee14592366be7641cb84519
Lenon Marcel <lenon.marcel@gmail.com>
2017-04-18 00:46:31 -0300

    Disable bold fonts

--- settings/font-settings.el
+++ settings/font-settings.el
@@ -2,6 +2,8 @@
     (set-frame-font "DejaVu Sans Mono-15" t t) ; bigger font for retina displays
   (set-frame-font "DejaVu Sans Mono-12" t t))
 
-(set-face-bold 'bold nil) ; disable bold fonts
+;; Disable bold fonts
+(dolist (face (face-list))
+  (set-face-attribute face nil :weight 'normal))
 
 (provide 'font-settings)
