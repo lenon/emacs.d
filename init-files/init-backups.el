009f0dddb794ad3cf720600337f85db23b0963af
Lenon Marcel <lenon.marcel@gmail.com>
2017-04-20 00:58:31 -0300

    Put all auto-saves into the same folder

--- init-files/init-backups.el
+++ init-files/init-backups.el
@@ -1,10 +1,20 @@
 (defvar tmp-dir (expand-file-name "tmp" user-emacs-directory))
+(defvar auto-save-file (expand-file-name ".auto-save-file-" tmp-dir))
 (defvar backups-dir (expand-file-name "backups" tmp-dir))
+(defvar auto-save-dir (expand-file-name "auto-save" tmp-dir))
 
-(when (not (file-exists-p backups-dir))
-  (mkdir backups-dir t)) ; creates a dir for backups if it does not exist
+;; Ensure backups dir and auto-save dir exist
+(dolist (dir `(,backups-dir
+               ,auto-save-dir))
+  (when (not (file-exists-p dir))
+    (mkdir dir t)))
 
-;; Save all backups into the same dir
+;; Save all backups into the same dir (do not create ~ files everywhere)
 (setq backup-directory-alist `(("." . ,backups-dir)))
+
+;; Auto-save files into the same dir (do not create #.*# files everywhere)
+(setq auto-save-file-name-transforms
+      `((".*" ,(concat auto-save-dir "/") t)))
+(setq auto-save-list-file-prefix auto-save-file)
 
 (provide 'init-backups)
