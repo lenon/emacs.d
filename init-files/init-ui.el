8487c3b6b38db69c8f511a15e3b548ddc69009a0
Lenon Marcel <lenon.marcel@gmail.com>
2017-04-21 02:43:10 -0300

    Mode-line tweaks

--- init-files/init-ui.el
+++ init-files/init-ui.el
@@ -1,3 +1,6 @@
+;; Check if current system is a Mac
+(defconst emacsd-is-a-mac (eq system-type 'darwin))
+
 (setq inhibit-startup-screen t) ; hide the welcome screen
 
 ;; TODO: Disable the startup message in the echo area
@@ -15,7 +18,7 @@
 
 (setq ring-bell-function 'ignore) ; turn off audible bell and visual bell
 
-(if (eq system-type 'darwin)
+(if emacsd-is-a-mac
     (set-frame-font "DejaVu Sans Mono-15" t t) ; bigger font for retina displays
   (set-frame-font "DejaVu Sans Mono-12" t t))
 
@@ -25,5 +28,19 @@
 
 ;; Start Emacs maximized
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; Show the current line and column number in the mode-line
+(line-number-mode)
+(column-number-mode)
+
+;; Show line and column number like this "123:456" in the mode-line
+;; Also removes the "All/Top/Bot" indicator
+(setq mode-line-position
+      '((line-number-mode ("%l" (column-number-mode ":%c")))))
+
+;; Use a smaller font for mode-line
+(dolist (face '(mode-line
+                mode-line-inactive))
+  (set-face-attribute face nil :font "DejaVu Sans Mono-13"))
 
 (provide 'init-ui)
