160f26b8673f9b5de0dda33cc9fedca5b5772a8f
Lenon Marcel <lenon.marcel@gmail.com>
2017-04-24 23:57:50 -0300

    Disable the error buffer

--- init-files/init-clojure.el
+++ init-files/init-clojure.el
@@ -10,7 +10,12 @@
 (add-hook 'cider-mode-hook #'eldoc-mode)
 (add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
 
-(setq cider-use-overlays nil)
+;; Disable the error buffer, it annoys me when neotree is open
+;; TODO: investigate why the error buffer is presented on a new frame
+;; when neotree is open
+(setq cider-show-error-buffer nil)
+
+;; Display results on the echo area
 (setq cider-overlays-use-font-lock t)
 
 (provide 'init-clojure)
