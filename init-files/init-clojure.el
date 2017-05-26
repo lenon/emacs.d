8ee8983d804a331cfd3eafccbf3106e004692c4f
Lenon Marcel <lenon.marcel@gmail.com>
2017-05-26 00:36:09 -0300

    Some tweaks for clojure files

--- init-files/init-clojure.el
+++ init-files/init-clojure.el
@@ -1,6 +1,6 @@
 (require 'clojure-mode)
 
-;; Indent fact and facts like this:
+;; Indent midje functions like this:
 ;; (fact "xpto"
 ;;   (something) => true)
 (dolist (midje-statement '(facts
@@ -13,13 +13,13 @@
 (add-hook 'cider-mode-hook #'eldoc-mode)
 (add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
 
-;; Disable the error buffer, it annoys me when neotree is open
-;; TODO: investigate why the error buffer is presented on a new frame
+;; Disable the error buffer because it is presented on a new frame
 ;; when neotree is open
+;; TODO: investigate this behavior
 (setq cider-show-error-buffer nil)
 
 ;; Display results on the echo area
-(setq cider-overlays-use-font-lock t)
+(setq cider-use-overlays nil)
 
 ;; Do not truncate lines on repl
 (with-eval-after-load 'cider-repl-mode-hook
