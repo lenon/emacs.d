4ea6da7ecc23cb847a73fbe65d62e06b29b5a509
Lenon Marcel <lenon.marcel@gmail.com>
2017-05-29 22:27:16 -0300

    Change neotree font size

--- init-files/init-faces.el
+++ init-files/init-faces.el
@@ -1,13 +1,13 @@
 (require 'emacsd)
 
 ;; Default font
-(defconst emacsd-default-font
+(defconst emacsd/default-font
   (if (emacsd/system-is-a-mac?)
       "DejaVu Sans Mono-15" ; bigger font for retina displays
     "DejaVu Sans Mono-12"))
 
 ;; Set the default font
-(set-frame-font emacsd-default-font t t)
+(set-frame-font emacsd/default-font t t)
 
 ;; Disable bold fonts
 (dolist (face (face-list))
--- init-files/init-mode-line.el
+++ init-files/init-mode-line.el
@@ -1,18 +1,19 @@
 (require 'emacsd)
 
 ;; Font for mode-line
-(defconst emacsd-mode-line-font-size
+(defconst emacsd/mode-line-font-size
   (if (emacsd/system-is-a-mac?) 13 12))
 
 ;; Background color
-(defconst emacsd-mode-line-background "dark violet")
+(defconst emacsd/mode-line-background "dark violet")
 
 ;; Color for text
-(defconst emacsd-mode-line-foreground "white")
+(defconst emacsd/mode-line-foreground "white")
 
 ;; Change mode-line font size
-(dolist (face '(mode-line mode-line-inactive))
-  (set-face-attribute face nil :height (* 10 emacsd-mode-line-font-size)))
+(dolist (face '(mode-line
+                mode-line-inactive))
+  (set-face-attribute face nil :height (* 10 emacsd/mode-line-font-size)))
 
 ;; Show the current line and column number in the mode-line
 (line-number-mode)
@@ -24,7 +25,7 @@
       '((line-number-mode ("%l" (column-number-mode ":%c")))))
 
 ;; Change mode-line colors
-(set-face-attribute 'mode-line nil :background emacsd-mode-line-background)
-(set-face-attribute 'mode-line nil :foreground emacsd-mode-line-foreground)
+(set-face-attribute 'mode-line nil :background emacsd/mode-line-background)
+(set-face-attribute 'mode-line nil :foreground emacsd/mode-line-foreground)
 
 (provide 'init-mode-line)
