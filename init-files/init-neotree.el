3ed40de69179836e0da27e5cf6ccc34502add960
Lenon Marcel <lenon.marcel@gmail.com>
2017-05-29 21:52:00 -0300

    Some neotree tweaks

--- init-files/init-neotree.el
+++ init-files/init-neotree.el
@@ -1,4 +1,12 @@
 (require 'neotree)
+
+;; Key binding to toggle Neotree
 (global-set-key (kbd "C-c n") 'neotree-toggle)
+
+;; Change Neotree theme
+(setq neo-theme 'nerd)
+
+;; Hide Neotree modeline
+(setq neo-mode-line-type 'none)
 
 (provide 'init-neotree)
