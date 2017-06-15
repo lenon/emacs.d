ee4eea38bbe1b339d4663eff1cf60a0449abb8dd
Lenon Marcel <lenon.marcel@gmail.com>
2017-06-15 00:28:43 -0300

    Change init-backups docs

--- init-files/init-backups.el
+++ init-files/init-backups.el
@@ -1,35 +1,37 @@
-;; Directory used for temporary files
 (defconst emacsd/tmp-dir
-  (expand-file-name "tmp" user-emacs-directory))
+  (expand-file-name "tmp" user-emacs-directory)
+  "The directory used for temporary files.")
 
-;; Directory used for backups
 (defconst emacsd/backups-dir
-  (expand-file-name "backups" emacsd/tmp-dir))
+  (expand-file-name "backups" emacsd/tmp-dir)
+  "The directory used for backups.")
 
-;; Directory used for auto-save files
 (defconst emacsd/auto-save-dir
-  (expand-file-name "auto-save" emacsd/tmp-dir))
+  (expand-file-name "auto-save" emacsd/tmp-dir)
+  "The directory used for auto-save files.")
 
-;; Prefix used for auto-save lists
 (defconst emacsd/auto-save-prefix
-  (expand-file-name ".auto-save-list-" emacsd/auto-save-dir))
+  (expand-file-name ".auto-save-list-" emacsd/auto-save-dir)
+  "Prefix used for auto-save lists.")
 
 ;; Ensure that backups and auto-save dirs exist
 (mkdir emacsd/backups-dir t)
 (mkdir emacsd/auto-save-dir t)
 
-;; Save all backups into the same dir
-;; Prevent emacs from creating ~* files everywhere
+;; Save all backups into the same directory and prevent Emacs from
+;; creating ~* files everywhere.
 (setq backup-directory-alist `(("." . ,emacsd/backups-dir)))
 
-;; Auto-save files into the same dir
-;; Prevent emacs from creating #.*# files everywhere
+;; Create auto-save files into the same directory and prevent Emacs from
+;; creating #.*# files everywhere.
 (setq auto-save-file-name-transforms
       `((".*" ,(concat emacsd/auto-save-dir "/") t)))
+
+;; Change where emacs should write the list of all auto-save file names.
 (setq auto-save-list-file-prefix emacsd/auto-save-prefix)
 
-;; Disable interlock files used to protect against simultaneous editing
-;; Prevent emacs from creating .#* symlinks
+;; Disable interlock files used to protect against simultaneous editing.
+;; Prevent emacs from creating .#* symlinks everywhere.
 (setq create-lockfiles nil)
 
 (provide 'init-backups)
